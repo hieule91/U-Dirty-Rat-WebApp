833c694b1f39c775fc049ea3925a0df3a875d423
jackle91 <hieu6@gatech.edu>
2017-11-19 15:53:32 -0500

    initial WebApp for U-Dirty-Rat

---
+++ U-Dirty-Rat/src/main/java/com/jjmae/UDirtyRat/UDirtyRatApplication.java
@@ -0,0 +1,12 @@
+package com.jjmae.UDirtyRat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UDirtyRatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UDirtyRatApplication.class, args);
+	}
+}
